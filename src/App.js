8288ae644e44694c57b7c662bd8d8c01821ee862
remziusta <remziusta.mdbf15@iste.edu.tr>
2021-04-01 12:43:09 +0300

    Using basic react-redux

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Counter from "./components/Counter";
+import DecreaseCounter from "./components/DecreaseCounter";
+import IncreaseCounter from "./components/IncreaseCounter";
+import IncreaseByTwoCounter from "./components/IncreaseByTwoCounter";
+function App() {
+  return (
+    <div>
+        <Counter/>
+        <DecreaseCounter/>
+        <IncreaseCounter/>
+        <IncreaseByTwoCounter/>
+    </div>
+  );
+}
+
+export default App;
